0d0b6609d63a41bb2fefe1d918f5051f7b0c0536
Joyal8714 <joyal4309@gmail.com>
2025-07-24 17:39:20 +0530

    changed

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 // Enable CORS
 app.use(cors());
+app.use(express.static('public'));
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/book-manager', {
